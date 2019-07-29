3287cda95e8673d330d8243c42c165365c4ba043
Recardo Poole <recardo.poole@gmail.com>
2019-07-29 11:05:43 -0600

    updated images

--- capstone/client/apparel/src/cart/Cart.vue
+++ capstone/client/apparel/src/cart/Cart.vue
@@ -29,8 +29,7 @@
         <div v-if="!isShowingCart" id="products" class="row">
           <div v-for="product in products" :key="product" :class="groupWrapper" class=" col-xs-4 col-lg-4">
             <div class="thumbnail"> 
-              <img v-for="image in images" :key="image" class="group list-group-image" :src=image.imageURL alt="" />
-              <!-- <img class="group list-group-image" :src="product.portrait1_url" alt="" /> -->
+              <img class="group list-group-image" :src="product.portrait1_url" alt="" />
                 <div class="caption">
                   <h4 class="group inner list-group-item-heading">
                     {{product.title}}
@@ -162,7 +161,66 @@
           items: []
         },
         name: true,
-        products: [],
+        products: [
+          {
+        id: 1,
+        title: 'Nuthin But A G Thang',
+        description: 'As a member of the g55 cohort at Galvanize, this shirt represents the hard work my classmates and I put in to become Full Stack Developers!',
+        price: 25.00,
+        inStock: 20,
+        portrait1_url: 'http://bit.ly/2ZhqRuZ',
+        portrait2_url: '',
+        portrait3_url: ''
+      },{
+        id: 2,
+        title: 'Monkey Business',
+        description: 'A fun ,colorful, creative t-shirt that will go with any casual wear.',
+        price: 25.00,
+        inStock: 20,
+        portrait1_url: 'http://bit.ly/32XA3a4',
+        portrait2_url: '',
+        portrait3_url: ''
+      },{
+        id: 3,
+        title: 'JIMI',
+        description: 'The Rock and Roll Hall of Fame describes him as "arguably the greatest instrumentalist in the history of rock music".',
+        price: 25.00,
+        inStock: 20,
+        portrait1_url: 'http://bit.ly/2ZiVBLW',
+        portrait2_url: '',
+        portrait3_url: ''
+      },{
+        id: 4,
+        title: 'Legendary Miles',
+        description: 'Miles Davis is synonymous with great Jazz. Here we are highlighting his signature hand position.',
+        price: 25.00,
+        inStock: 20,
+        portrait1_url: 'http://bit.ly/2YyyXSC',
+        portrait2_url: '',
+        portrait3_url: ''
+      },{
+        id: 5,
+        title: 'Mile Carter Collection',
+        description: 'A clean elegant design that clearly identifies the owner and communicates class!',
+        price: 25.00,
+        inStock: 20,
+        portrait1_url: 'http://bit.ly/2ylwZ9v',
+        portrait2_url: '',
+        portrait3_url: ''
+      },{
+        id: 6,
+        title: 'Native Detroiter',
+        description: 'I wanted to design something that would represent the pride and love that Detroiters have for their beloved city.',
+        price: 25.00,
+        inStock: 20,
+        portrait1_url: 'http://bit.ly/2K8ZsF7',
+        portrait2_url: '',
+        portrait3_url: ''
+
+
+
+      }
+    ],
         size: [],
         selected: []
       }
@@ -196,6 +254,9 @@
           minimumFractionDigits: 2
         });
         return formatter.format(value);
+      },
+      shirtImage () {
+        this.images[this.imageVariant].imageURL;        
       }
     },
     methods:{
@@ -205,18 +266,18 @@
       }
       product.selected.push(size)
     },
-    fetchItems() {
-      this.$http.get('https://miles-carter.herokuapp.com/')
-      // this.$http.get('http://localhost:8080/')
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-          for (let key in data) {
-            this.products.push(data[key])
-          }
-        })
-      },
+    // fetchItems() {
+    //   this.$http.get('https://miles-carter.herokuapp.com/')
+    //   // this.$http.get('http://localhost:8080/')
+    //     .then(response => {
+    //       return response.json()
+    //     })
+    //     .then(data => {
+    //       for (let key in data) {
+    //         this.products.push(data[key])
+    //       }
+    //     })
+    //   },
       fetchSizes() {
         this.$http.get('https://miles-carter.herokuapp.com/size')
           .then(response => {
