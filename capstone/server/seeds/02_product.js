55e44ee5dff401e1687cd29fa914c0ceb9f229ff
Recardo Poole <recardo.poole@gmail.com>
2019-07-28 19:06:49 -0600

    update the image files

--- capstone/server/seeds/02_product.js
+++ capstone/server/seeds/02_product.js
@@ -10,7 +10,7 @@ exports.seed = function(knex, Promise) {
         description: 'As a member of the g55 cohort at Galvanize, this shirt represents the hard work my classmates and I put in to become Full Stack Developers!',
         price: 25.00,
         inStock: 20,
-        portrait1_url: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/23131940_10155859147168748_6559088587689980013_n.jpg?oh=6b9e47b81c03250fccd9b15f0d9b8f17&oe=5AA1D0A3',
+        portrait1_url: 'http://bit.ly/2ZhqRuZ',
         portrait2_url: '',
         portrait3_url: ''
       },{
@@ -19,7 +19,7 @@ exports.seed = function(knex, Promise) {
         description: 'A fun ,colorful, creative t-shirt that will go with any casual wear.',
         price: 25.00,
         inStock: 20,
-        portrait1_url: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22885923_10155859147173748_6131228126796314514_n.jpg?oh=6155830f99d2dc2a52dfaf6ad0d0ef5e&oe=5AAB9FFF',
+        portrait1_url: 'http://bit.ly/32XA3a4',
         portrait2_url: '',
         portrait3_url: ''
       },{
@@ -28,7 +28,7 @@ exports.seed = function(knex, Promise) {
         description: 'The Rock and Roll Hall of Fame describes him as "arguably the greatest instrumentalist in the history of rock music".',
         price: 25.00,
         inStock: 20,
-        portrait1_url: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22894325_10155859147108748_6202913189106914146_n.jpg?oh=032e83a703b99e87d3ba027f1047e66f&oe=5AA0E883',
+        portrait1_url: 'http://bit.ly/2ZiVBLW',
         portrait2_url: '',
         portrait3_url: ''
       },{
@@ -37,7 +37,7 @@ exports.seed = function(knex, Promise) {
         description: 'Miles Davis is synonymous with great Jazz. Here we are highlighting his signature hand position.',
         price: 25.00,
         inStock: 20,
-        portrait1_url: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/23032388_10155859147088748_4237818319702161896_n.jpg?oh=7d48723f37f0a708b35226a7cccb6414&oe=5A778732',
+        portrait1_url: 'http://bit.ly/2YyyXSC',
         portrait2_url: '',
         portrait3_url: ''
       },{
@@ -46,7 +46,7 @@ exports.seed = function(knex, Promise) {
         description: 'A clean elegant design that clearly identifies the owner and communicates class!',
         price: 25.00,
         inStock: 20,
-        portrait1_url: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22886079_10155859147083748_2513273925417262028_n.jpg?oh=22d66c6bc636369948828cad966ea10a&oe=5AA6A318',
+        portrait1_url: 'http://bit.ly/2ylwZ9v',
         portrait2_url: '',
         portrait3_url: ''
       },{
@@ -55,7 +55,7 @@ exports.seed = function(knex, Promise) {
         description: 'I wanted to design something that would represent the pride and love that Detroiters have for their beloved city.',
         price: 25.00,
         inStock: 20,
-        portrait1_url: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/23167888_10155859147103748_8860916604472817923_n.jpg?oh=ccf0d53b6410ccaddf529ec89dbc84b1&oe=5AA12A9C',
+        portrait1_url: 'http://bit.ly/2K8ZsF7',
         portrait2_url: '',
         portrait3_url: ''
 
